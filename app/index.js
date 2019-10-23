28bc166146bc0311b5e5887e4e5f30e31ffaac2f
Paul D. Duncan <chesiredarling@gmail.com>
2019-10-23 16:22:06 -0700

    Production build

--- app/index.js
+++ app/index.js
@@ -2,18 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
+import { ThemeProvider } from './contexts/Theme'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className='container'>
-        <Popular />
-      </div>
-    )
-  }
-}
+  constructor(props) {
+    super(props)
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+  render()
+{
+  return (
+    <ThemeProvider value ={this.state}>
+      <div className='container'>
+        <Battle />
+      </div>
+    </ThemeProvider>
+  )
+}}
